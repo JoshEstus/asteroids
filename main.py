a645b91570dd2b44f37ca7d84bafed2642dc77c5
JoshEstus <josh.estus@gmail.com>
2025-04-17 14:00:13 -0400

    Actually group player correctly

--- main.py
+++ main.py
@@ -9,19 +9,20 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
 
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2 )
+
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    player.containers(updateable, drawable)
+    Player.containers = (updateable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2 )
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
-        updateable.update()
+        updateable.update(dt)
         for draw in drawable:
-            draw.draw()
+            draw.draw(screen)
         pygame.display.flip()
         delta = clock.tick(60)
         dt = delta/1000
